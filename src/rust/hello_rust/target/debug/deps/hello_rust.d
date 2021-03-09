5c5ed782339d184fa0d235dba65f77cd2c5c8568
Sam Liu <ontouchstart@gmail.com>
2021-03-09 18:01:16 -0500

    ignore rust/*/target

--- src/rust/hello_rust/target/debug/deps/hello_rust.d
+++
@@ -1,5 +0,0 @@
-/Users/sam/github/cloudflare/pages.dev/cloudflare-page/src/rust/hello_rust/target/debug/deps/hello_rust: src/main.rs
-
-/Users/sam/github/cloudflare/pages.dev/cloudflare-page/src/rust/hello_rust/target/debug/deps/hello_rust.d: src/main.rs
-
-src/main.rs:
