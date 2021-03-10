611c69524c2f729cf0e7bd49b36c9e3b411c339c
Sam Liu <ontouchstart@gmail.com>
2021-03-09 21:13:08 -0500

    remove book/*/target

--- book/chapter_2.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const api = 'https://ontouchstart-rustwasm-markdown-parser.ontouchstart.workers.dev/';
-    const output_html = await (
-        await fetch(api)
-    ).text();
-    document.getElementById('output').innerHTML = output_html;
-})();
--- book/wasm/link_section/src/lib.rs
+++
@@ -1,2 +0,0 @@
-#[link_section = "data"]
-pub static SECTION: [u8; 12] = *b"Hello World!";
