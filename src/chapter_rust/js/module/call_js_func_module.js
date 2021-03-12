5cc2e87d9418138b84cc88ddcd66ccd970a3a26a
Sam Liu <ontouchstart@gmail.com>
2021-03-12 10:37:34 -0500

    js/call_js_func_module.js vs js/module/call_js_func_module.js

--- src/chapter_rust/js/module/call_js_func_module.js
+++ src/chapter_rust/js/module/call_js_func_module.js
@@ -3,8 +3,19 @@ import init, {show_add} from '/chapter_rust/rust/call_js_func_module/pkg/call_js
 (async ()=> {
     const result = await init();
     console.log({result})
+    const { memory } = result;
+    console.log ( { memory });
+    const { buffer }  = memory;
+    console.log({ buffer });
+    console.log(buffer.byteLength);
+
     const i32max = (0xffffffff - 1) / 2;
-    show_add(1,1);
+    document.getElementById(`call_js_func_module_output`).innerHTML += `
+
+FROM chapter/js/module/call_js_func_modules.js
+Memory buffer ${buffer.byteLength} bytes
+`
+    show_add(1,2);
     show_add(3,4);
     show_add(i32max, 1);
 })();
