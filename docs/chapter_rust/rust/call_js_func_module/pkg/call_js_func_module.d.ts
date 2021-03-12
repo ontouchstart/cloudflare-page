add03539266f72b42aa536f3d0dd12aff9a5dd7e
Sam Liu <ontouchstart@gmail.com>
2021-03-12 09:22:09 -0500

    keep chapter_rust/rust/call_js_func_module/pkg

---
+++ docs/chapter_rust/rust/call_js_func_module/pkg/call_js_func_module.d.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} x
+* @param {number} y
+*/
+export function show_add(x: number, y: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly show_add: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
